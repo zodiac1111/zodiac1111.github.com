ef1e0a4c17ef2683b4c796bd34ef510ff0ff38e7
zodiac1111 <zodiac8765+github_admin@gmail.com>
2012-07-03 18:09:27 +0800

    blog source update

--- source/_posts/2012-07-03-hello-world.markdown
+++ source/_posts/2012-07-03-hello-world.markdown
@@ -5,3 +5,4 @@ date: 2012-07-03 09:01
 comments: true
 categories: 
 ---
+测试程序:{% include_code 1.c %}
---
+++ source/downloads/code/1.c
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+#define STRCPY(a, b) while(*a++ = *b++);
+void __attribute__((stdcall)) f1(int a,int b)
+{
+	;
+}
+typedef int(*p)(int) ;
+void foo(void) 
+{ 
+	int i=0x12345678;
+	int j=0x12345678;
+	unsigned char *p=(unsigned char *)&i;
+	unsigned char *q=(unsigned char *)&j;
+	q>NULL;
+	//for(l)
+	printf("%x\n",*p);
+	printf("%p\n",p);
+	p++;
+
+}
+int main()                                                                       
+{
+	foo();	
+}  
